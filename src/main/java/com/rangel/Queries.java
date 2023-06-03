067aee888574fe1a9ca7dc868a7a40dd5aa71ab3
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-02 23:57:44 -0300

    Added a doc for the method ActorsWhoActedInDirectorsMoviesAlphabetically in Queries class

--- src/main/java/com/rangel/Queries.java
+++ src/main/java/com/rangel/Queries.java
@@ -21,7 +21,7 @@ public class Queries {
     * Query 1: from the movies collection in this class, this method returns the set 
     * of actors who played themselves in at least one of these movies.
     *
-    * It is considered that "actors who played themselves" those who have their names as a key 
+    * <p>It is considered that "actors who played themselves" those who have their names as a key 
     * on the Map `actorsPerCharacter` and also as one of the items belonging to the set 
     * associated with this same key.</p>
     */
@@ -30,13 +30,13 @@ public class Queries {
     }
 
     /**
-    * Consulta 2: a partir da coleção de filmes desta classe, este método retorna a lista de atores
-    * que atuaram em pelo menos um filme de um determinado diretor. A lista retornada está disposta
-    * em ordem alfabética.
+    * Query 2: from the movies collection in this class, this method returns a list of actors 
+    * who played in at least one movie of a particular director. The list returned must be
+    * in alphabetical order.
     *
-    * <p>Considera-se que um ator tenha atuado em um filme de um determinado diretor se ele tem o
-    * seu nome como um dos itens do campo `atores`, ao mesmo tempo em que o diretor em questão
-    * tem o seu nome como um dos itens do campo `diretores` do mesmo filme.</p>
+    * <p>It is considered that an actor has played in a certain movie of a certain director if they have their
+    * names as one of the items on the field `actors` as well as the director in this case 
+    * has their names as one of the items on the field `directors` in the same movies.</p>
     */
     public List<String> ActorsWhoActedInDirectorsMoviesAlphabetically(String director) {
         return emptyList(); // TODO: Implement.
