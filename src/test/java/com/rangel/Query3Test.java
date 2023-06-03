c8e87fe352c825a1f382a9f4a81e488fdb4e70fb
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-03 12:49:23 -0300

    TDD test for query 3 returning an empty list when passing an empty set

---
+++ src/test/java/com/rangel/Query3Test.java
@@ -0,0 +1,27 @@
+package com.rangel;
+
+import static java.util.Collections.emptySet;
+import static java.util.Collections.emptyList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Query3Test {
+    
+    @Test
+    @DisplayName("Query 3 - querying based on an empty collection must return an empty list")
+    public void queryingBasedOnAnEmptyCollection() {
+        testQuery(emptySet(), emptyList());
+    }
+
+    private static void testQuery(Collection<Movie> movies, List<Movie> expectedOutput) {
+        Queries queries = new Queries(movies);
+        List<Movie> actualOutput = queries.moviesInWhichAtLeastOneDirectorActedMostRecentFirst();
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
