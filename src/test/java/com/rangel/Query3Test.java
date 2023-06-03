d18443e305643c859fdfd8bff38e8093120487dc
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2023-06-03 13:17:59 -0300

    TDD test for query 3 returning a list with not duplicate items

--- src/test/java/com/rangel/Query3Test.java
+++ src/test/java/com/rangel/Query3Test.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class Query3Test {
-    
+
     @Test
     @DisplayName("Query 3 - querying based on an empty collection must return an empty list")
     public void queryingBasedOnAnEmptyCollection() {
@@ -23,18 +23,28 @@ public class Query3Test {
     @DisplayName("Query 3 - the query must return an empty list when no movie fulfills the requirement")
     public void noMovieFulfillsTheRequirement() {
         testQuery(
-            Set.of(Movies.spaceJam2021, Movies.spiderManNoWayHome, Movies.djangoUnchained),
-            emptyList()
-        );
+                Set.of(Movies.spaceJam2021, Movies.spiderManNoWayHome, Movies.djangoUnchained),
+                emptyList());
     }
 
     @Test
     @DisplayName("Query 3 - the query must return the correct result in the correct order")
     public void aFewMoviesFulfillTheRequirement() {
         testQuery(
-            Movies.all(),
-            List.of(Movies.rockyII, Movies.braveheart, Movies.theGreatDictator)
-        );
+                Movies.all(),
+                List.of(Movies.rockyII, Movies.braveheart, Movies.theGreatDictator));
+    }
+
+    @Test
+    @DisplayName("Query 3 - no movies must appear more than once on the result list")
+    public void noDuplicateItems() {
+        List<Movie> movies = List.of(Movies.braveheart, Movies.braveheart);
+        Queries queries = new Queries(movies);
+        List<Movie> results = queries.moviesInWhichAtLeastOneDirectorActedMostRecentFirst();
+
+        long countingBraveHeart = results.stream()
+                .filter(movie -> movie.equals(Movies.braveheart))
+                .count();
     }
 
     private static void testQuery(Collection<Movie> movies, List<Movie> expectedOutput) {
